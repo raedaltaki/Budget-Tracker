f9de21a27ec893451a1ba27b0edfeb4bdb95b551
raedaltaki <raed.simon@gmail.com>
2021-04-14 18:06:05 -0400

    add indexeddb

---
+++ public/js/idb.js
@@ -0,0 +1,83 @@
+const { get } = require("mongoose");
+
+let db;
+
+const request = indexedDB.open('budget_tracker',1);
+
+request.onupgradeneeded = function(event)
+{
+    const db = event.target.result;
+    db.createObjectStore('new_deposit',{autoIncrement: true});
+};
+
+request.onsuccess = function(event)
+{
+    db = event.target.result;
+    if(navigator.onLine)
+    {
+        uploadRecord();
+    }  
+};
+
+request.onerror = function(event) 
+{
+    console.log(event.target.errorCode);
+};
+
+function saveRecord(record)
+{
+    const transaction = db.transaction(['new_deposit'],'readwrite');
+
+    const depositObject = transaction.objectStore('new_deposit');
+
+    depositObject.add(record);
+}
+
+function uploadRecord(record)
+{
+    const transaction = db.transaction(['new_deposit'],'readwrite');
+
+    const depositObject = transaction.objectStore('new_deposit');
+
+    const getAll = depositObject.getAll();
+
+    getAll.onsuccess = function()
+    {
+        if(getAll.result.length)
+        {
+            fetch("/api/transaction", {
+                method: "POST",
+                body: JSON.stringify(transaction),
+                headers: {
+                  Accept: "application/json, text/plain, */*",
+                  "Content-Type": "application/json"
+                }
+            })
+            .then(response => 
+            {    
+                return response.json();
+            })
+            .then(data => 
+            {
+                if (data.message) 
+                {
+                    throw new Error(data);
+                }
+
+                const transaction = db.transaction(['new_deposit'],'readwrite');
+
+                const depositObject = transaction.objectStore('new_deposit');
+
+                depositObject.clear();
+
+                alert('All saved deposits have been submitted!');
+            })
+            .catch(err => 
+            {
+                console.log(err);
+            });
+        }
+    };
+}
+
+window.addEventListener('online', uploadRecord);
